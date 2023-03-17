9a4f02d0a603da17baf76b874ea32521b05a74ce
rgdl <robert_delisle44@hotmail.com>
2023-03-17 22:33:59 +1000

    add some basic methods and tests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,94 @@
-type NoteCollection = Vec<u8>;
+use std::collections::HashSet;
+
+type NoteCollection = Vec<i8>;
+
+fn rotate(notes: NoteCollection) -> NoteCollection {
+    let shift_amount = notes[1];
+    let mut shifted_notes = notes
+        .iter()
+        .map(|note| (note - shift_amount) % 12)
+        .map(|note| if note < 0 { note + 12 } else { note } )
+        .collect::<NoteCollection>();
+    shifted_notes.sort();
+    shifted_notes
+}
 
 #[derive(Debug)]
 struct Scale {
     notes: NoteCollection,
+    modes: HashSet<NoteCollection>,
 }
 
 impl Scale {
     fn new(notes: NoteCollection) -> Scale {
-        Scale { notes }
+        let mut mode = notes.clone();
+        let mut modes = HashSet::new();
+        modes.insert(mode.clone());
+        loop {
+            mode = rotate(mode);
+            if mode == notes {
+                break;
+            }
+            modes.insert(mode.clone());
+        }
+        Scale { notes, modes }
+    }
+
+    fn equals(&self, other: &Scale) -> bool {
+        self.notes == other.notes
+    }
+
+    fn is_mode_of(&self, other: &Scale) -> bool {
+        self.modes.intersection(&other.modes).count() > 0
     }
 }
 
 fn main() {
     let major = Scale::new(vec!(0, 2, 4, 5, 7, 9, 11));
     println!("This is the major scale: {:?}", major);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scale_modes_generated_correctly() {
+        let major = Scale::new(vec!(0, 2, 4, 5, 7, 9, 11));
+        assert_eq!(
+            major.modes,
+            HashSet::from([
+                vec!(0, 2, 4, 5, 7, 9, 11),
+                vec!(0, 2, 3, 5, 7, 9, 10),
+                vec!(0, 1, 3, 5, 7, 8, 10),
+                vec!(0, 2, 4, 6, 7, 9, 11),
+                vec!(0, 2, 4, 5, 7, 9, 10),
+                vec!(0, 2, 3, 5, 7, 8, 10),
+                vec!(0, 1, 3, 5, 6, 8, 10),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_scale_equals_itself() {
+        let major = Scale::new(vec!(0, 2, 4, 5, 7, 9, 11));
+        assert!(major.equals(&major));
+    }
+
+    #[test]
+    fn test_dorian_is_mode_of_major() {
+        let major = Scale::new(vec!(0, 2, 4, 5, 7, 9, 11));
+        let dorian = Scale::new(vec!(0, 2, 3, 5, 7, 9, 10));
+        assert!(major.is_mode_of(&dorian));
+        assert!(dorian.is_mode_of(&major));
+    }
+
+    #[test]
+    fn test_melodic_minor_is_not_major() {
+        let major = Scale::new(vec!(0, 2, 4, 5, 7, 9, 11));
+        let melodic_minor = Scale::new(vec!(0, 2, 3, 5, 7, 9, 11));
+        assert!(!major.equals(&melodic_minor));
+        assert!(!major.is_mode_of(&melodic_minor));
+    }
 }
